6fceea5cad1fa5b9c07be110126db0d598951feb
Prangya P Dehury <162728036+PPD-101@users.noreply.github.com>
2024-09-23 09:57:13 +0530

    Create addition.py
    Addition of Two Numbers as User Input

---
+++ addition.py
@@ -0,0 +1,5 @@
+# Mathematical Operation
+#Addition of two numbers (User Input)
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+print("Addition of",num1,"and",num2,"is:",num1+num2)
